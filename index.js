f347123869f52cd971f467d5862c43485040ac90
jishujana <126905216+jishujana@users.noreply.github.com>
2023-03-03 23:07:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick=function(){
+    navBar= document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
+
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_iv6bekcjana";
+    const templateID = "template_uycwxgx";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
